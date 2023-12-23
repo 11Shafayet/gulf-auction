ee7a0b817d44491cc23da0448b75829322b64573
Shafayet <11shafayet@gmail.com>
2023-12-23 14:31:04 +0600

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,64 @@ import {
   FaPhoneAlt,
   FaTwitterSquare,
 } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
 import logoWide from '/images/logo-wide.png';
 import google from '/images/google-button.png';
 import apple from '/images/apple-button.png';
-import { Link } from 'react-router-dom';
+
+const details = [
+  {
+    logo: <FaMapMarkerAlt />,
+    text: 'UBAI, UNITED ARAB EMIRATES',
+  },
+  {
+    logo: <FaPhoneAlt />,
+    text: '0581035772',
+  },
+  {
+    logo: <FaEnvelope />,
+    text: 'info@arianashippingline.com',
+  },
+];
+
+const socialData = [
+  {
+    logo: <FaFacebookSquare size={35} />,
+    link: `/`,
+  },
+  {
+    logo: <FaInstagramSquare size={35} />,
+    link: `/`,
+  },
+  {
+    logo: <FaTwitterSquare size={35} />,
+    link: `/`,
+  },
+];
+
+const resourcesData = [
+  {
+    text: 'Our Team',
+    link: '/',
+  },
+  {
+    text: 'Contact Us',
+    link: '/',
+  },
+  {
+    text: 'About',
+    link: '/',
+  },
+  {
+    text: 'Services',
+    link: '/',
+  },
+  {
+    text: 'Terms & Conditions',
+    link: '/',
+  },
+];
 
 const Footer = () => {
   return (
@@ -24,69 +78,44 @@ const Footer = () => {
               We sell perspiciatis unde omnis iste natus error sit voluptatem
               accusantium doloremque laudantium
             </p>
-            <ul className="flex flex-col gap-y-2 my-10">
-              <li className="flex items-center gap-x-2">
-                <FaMapMarkerAlt className="text-primary" />
-                <p className="uppercase opacity-80">
-                  DUBAI, UNITED ARAB EMIRATES
-                </p>
-              </li>
-              <li className="flex items-center gap-x-2">
-                <FaPhoneAlt className="text-primary" />
-                <p className="uppercase opacity-80">0581035772</p>
-              </li>
-              <li className="flex items-center gap-x-2">
-                <FaEnvelope className="text-primary" />
-                <p className="opacity-80">info@arianashippingline.com</p>
-              </li>
+            {/* details */}
+            <ul className="flex flex-col gap-y-3 my-10">
+              {details.map((detail, i) => (
+                <li className="flex items-center gap-x-2" key={i}>
+                  <div className="text-primary">{detail.logo}</div>
+                  <p className="uppercase opacity-80">{detail.text}</p>
+                </li>
+              ))}
             </ul>
+
+            {/* social */}
             <ul className="flex items-center gap-x-4">
-              <li>
-                <Link>
-                  <FaFacebookSquare
-                    className="rounded-md text-gray-700 hover:text-primary hover:-translate-y-2 duration-500"
-                    size={35}
-                  />
-                </Link>
-              </li>
-              <li>
-                <Link>
-                  <FaInstagramSquare
-                    className="rounded-md text-gray-700 hover:text-primary hover:-translate-y-2 duration-500"
-                    size={35}
-                  />
-                </Link>
-              </li>
-              <li>
-                <Link>
-                  <FaTwitterSquare
-                    className="rounded-md text-gray-700 hover:text-primary hover:-translate-y-2 duration-500"
-                    size={35}
-                  />
-                </Link>
-              </li>
+              {socialData.map((social, i) => (
+                <li key={i}>
+                  <Link to={social.link}>
+                    <div className="rounded-md text-gray-700 hover:text-primary hover:-translate-y-2 duration-500">
+                      {social.logo}
+                    </div>
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
+
           <div className="w-full md:w-2/3 flex flex-col sm:flex-row justify-between gap-8 mt-12 md:mt-20 md:pl-32">
+            {/* resources */}
             <div>
               <h4 className="text-2xl font-medium mb-3">Resources</h4>
               <div className="h-[3px] w-32 bg-primary" />
               <ul className="flex flex-col gap-y-4 mt-8">
-                <li className="text-lg font-medium hover:text-primary">
-                  <Link>Our Team</Link>
-                </li>
-                <li className="text-lg font-medium hover:text-primary">
-                  <Link>Contact Us</Link>
-                </li>
-                <li className="text-lg font-medium hover:text-primary">
-                  <Link>About</Link>
-                </li>
-                <li className="text-lg font-medium hover:text-primary">
-                  <Link>Services</Link>
-                </li>
-                <li className="text-lg font-medium hover:text-primary">
-                  <Link>Terms & Conditions</Link>
-                </li>
+                {resourcesData.map((item, i) => (
+                  <li
+                    className="text-lg font-medium hover:text-primary"
+                    key={i}
+                  >
+                    <Link to={item.link}>{item.text}</Link>
+                  </li>
+                ))}
               </ul>
             </div>
 
@@ -123,7 +152,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <hr className="my-6" />
+      <hr className="mt-6" />
       <div className="container mx-auto px-4 text-center py-6">
         <p>© Copyright 2023, All Rights Reserved</p>
       </div>
